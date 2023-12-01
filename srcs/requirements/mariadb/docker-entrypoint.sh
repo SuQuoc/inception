d9a31bb6b640b3952f43e3bbbffac558e76d0445
qtranInception <tran_quoc_su@hotmail.com>
2023-12-01 19:01:46 +0100

    commented mysql_install_db, cuz it might not be needed

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -8,34 +8,35 @@ fi
 #
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then
-    echo "Database already exists: /var/lib/mysql/$MYSQL_DATABASE"
-    cat /var/lib/mysql/$MYSQL_DATABASE
+  echo "Database already exists: /var/lib/mysql/$MYSQL_DATABASE"
 else
-  mysql_install_db
-  #mysqld
+  #mysql_install_db #might not be necessary since mariadb 10.2
+  
   #service mysql start
-
   #/etc/init.d/mariadb start
   /usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
   until mysqladmin ping 2> /dev/null; do
-        sleep 2
-    done
-
+    sleep 2
+  done
 
   echo "DOES THIS DOLLAR SHIT WORK?: '$MYSQL_ROOT_PASSWORD'"
   
   mysql -u root <<EOF
     CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
     
+    # Setup root PW since its without one right now
     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
     
+    # Removes root user entries that are configured to connect from hosts other than the local machine.
     DELETE FROM mysql.user WHERE user='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+    
+    # deletes user accounts from the mysql.user table where the username is an empty string ('').
     DELETE FROM mysql.user WHERE user='';
     
+    # Creating a user with PW with all privileges on DATABASE able to login from ANY IP-addr
     CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
     GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-    
     FLUSH PRIVILEGES;
 EOF
   killall mysqld 2> /dev/null
